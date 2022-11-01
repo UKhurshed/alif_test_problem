a0660af451856b65aeaf62aa01d6f7aa0525d97e
UKhurshed <khurshedumarov777@gmail.com>
2022-11-01 11:41:48 +0500

    Added russian locale to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:alif_test/app_bloc_observer.dart';
-import 'package:alif_test/features/top_headlines/ui/cubit/top_headlines_cubit.dart';
+
+import 'package:alif_test/features/top_headlines/ui/bloc/top_headline/top_headlines_bloc.dart';
 import 'package:alif_test/features/top_headlines/ui/screens/top_headlines_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:alif_test/core/injector_container/injector_container.dart'
+    as di;
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = AppBlocObserver();
+  await di.initIC();
   runApp(const MyApp());
 }
 
@@ -16,8 +20,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<TopHeadlinesCubit>(
-        create: (_) => TopHeadlinesCubit(),
-        child: const MaterialApp(home: TopHeadlinesScreen()));
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => TopHeadlinesBloc()),
+        ],
+        child: const MaterialApp(
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            home: TopHeadlinesScreen()));
   }
 }
